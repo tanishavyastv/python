34cd8bb192b26491bbca0ae00107f909aececa75
Tanisha Vyas <165962059+tanishavyastv@users.noreply.github.com>
2024-08-26 17:22:38 +0530

    Delete Q29 reverse number.py

--- Q29 reverse number.py
+++
@@ -1,8 +0,0 @@
-# reverse the digits of given number without using built-in function
-
-num = int(input('Enter a number: '))
-rev = ''
-while num > 0:
-    rev += str(num % 10)
-    num //= 10
-print(f'Reversed Number: {rev}')
