c984529af0f0e0a6089e5ce8da08fc9000c60e3d
Tanisha Vyas <165962059+tanishavyastv@users.noreply.github.com>
2024-10-09 03:25:48 +0530

    Delete Q34 exception handling 1.py

--- Q34 exception handling 1.py
+++
@@ -1,8 +0,0 @@
-#wap to demonstrate try and except (exception handling)
-try:
-    number = int(input('Enter a number: '))
-    result = 10 / number
-except ZeroDivisionError:
-    print('Error: Cannot divide by zero.')
-except ValueError:
-    print('Error: Invalid input. Please enter a valid number.')
